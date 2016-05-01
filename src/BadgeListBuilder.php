9a14f001a1e11725d39f41e5f4f8dc60cc9a7d55
Mohit Aghera <mhaghera_alpha@yahoo.co.in>
2016-05-01 14:00:27 +0530

    Adding implementation for table listing using tabledrag method.

--- src/BadgeListBuilder.php
+++ src/BadgeListBuilder.php
@@ -12,16 +12,13 @@ use Drupal\Core\Entity\EntityListBuilder;
 use Drupal\Core\Link;
 use Drupal\Core\Routing\LinkGeneratorTrait;
 use Drupal\Core\Url;
-use Drupal\Core\Form\FormBuilderInterface;
-use Drupal\Core\Form\FormInterface;
-use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Defines a class to build a listing of Badge entities.
  *
  * @ingroup user_badges
  */
-class BadgeListBuilder extends EntityListBuilder implements FormInterface{
+class BadgeListBuilder extends EntityListBuilder{
   use LinkGeneratorTrait;
 
   /**
@@ -72,68 +69,4 @@ class BadgeListBuilder extends EntityListBuilder implements FormInterface{
     );
     return $row + parent::buildRow($entity);
   }
-
-  /**
-   * {@inheritdoc}
-   *
-   * @param string|null $theme
-   *   (optional) The theme to display the blocks for. If NULL, the current
-   *   theme will be used.
-   * @param \Symfony\Component\HttpFoundation\Request $request
-   *   The current request.
-   *
-   * @return array
-   *   The block list as a renderable array.
-   */
-  public function render($user) {
-
-    return $this->formBuilder->getForm($this);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getFormId() {
-    return 'badge_arrange_form_form';
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['#attached']['library'][] = 'core/drupal.tableheader';
-    $form['#attached']['library'][] = 'block/drupal.block';
-    $form['#attached']['library'][] = 'block/drupal.block.admin';
-    $form['#attributes']['class'][] = 'clearfix';
-
-    // Build the form tree.
-
-
-    $form['actions'] = array(
-      '#tree' => FALSE,
-      '#type' => 'actions',
-    );
-    $form['actions']['submit'] = array(
-      '#type' => 'submit',
-      '#value' => $this->t('Save blocks'),
-      '#button_type' => 'primary',
-    );
-
-    return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    // No validation.
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-
-  }
-
 }
--- src/Controller/UserBadgesListBuilder.php
+++ src/Controller/UserBadgesListBuilder.php
@@ -9,8 +9,9 @@ namespace Drupal\user_badges\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\user\UserInterface;
-use Drupal\Core\Entity\Controller\EntityListController;
-use Drupal\Core\Extension\ThemeHandlerInterface;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Form\FormInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -18,23 +19,30 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *
  * @package Drupal\user_badges\Controller
  */
-class UserBadgesListBuilder extends EntityListController{
+class UserBadgesListBuilder extends ControllerBase implements FormInterface{
 
   /**
-   * The theme handler.
+   * The form builder.
    *
-   * @var \Drupal\Core\Extension\ThemeHandlerInterface
+   * @var \Drupal\Core\Form\FormBuilderInterface
    */
-  protected $themeHandler;
+  protected $formBuilder;
 
   /**
-   * Constructs the BlockListController.
+   * User object of currently being edited user.
    *
-   * @param \Drupal\Core\Extension\ThemeHandlerInterface $theme_handler
-   *   The theme handler.
+   * @var \Drupal\user\User
    */
-  public function __construct(ThemeHandlerInterface $theme_handler) {
-    $this->themeHandler = $theme_handler;
+  protected $user;
+
+  /**
+   * Constructs a new BlockListBuilder object.
+
+   * @param \Drupal\Core\Form\FormBuilderInterface $form_builder
+   *   The form builder.
+   */
+  public function __construct(FormBuilderInterface $form_builder) {
+    $this->formBuilder = $form_builder;
   }
 
   /**
@@ -42,7 +50,7 @@ class UserBadgesListBuilder extends EntityListController{
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('theme_handler')
+      $container->get('form_builder')
     );
   }
 
@@ -56,7 +64,90 @@ class UserBadgesListBuilder extends EntityListController{
    *   The block list as a renderable array.
    */
   public function badgesListing(UserInterface $user) {
-    return $this->entityManager()->getListBuilder('badge')->render($user);
+    $this->user = $user;
+    return $this->formBuilder->getForm($this);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'badge_display_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $entity_manager = \Drupal::entityManager();
+    $form['#attached']['library'][] = 'core/drupal.tableheader';
+    $form['#attributes']['class'][] = 'clearfix';
+    /** @var \Drupal\user\Entity\User $user */
+    $user = $this->user;
+
+    $form['badge_listing_table'] = array(
+      '#type' => 'table',
+      '#header' => array(t('Label'), t('Weight')),
+      '#empty' => t('There are no badges yet'),
+      '#tableselect' => TRUE,
+      '#tabledrag' => array(
+        array(
+          'action' => 'order',
+          'relationship' => 'sibling',
+          'group' => 'user-badges-order-weight',
+        ),
+      ),
+    );
+    $badges = $user->get('field_user_badges')->getValue();
+    foreach ($badges as $id => $badge_id) {
+      /** @var \Drupal\user_badges\Entity\Badge $badge */
+      $badge = $entity_manager->getStorage('badge')->load($badge_id['target_id']);
+      $form['badge_listing_table'][$id]['#attributes']['class'][] = 'draggable';
+      $form['badge_listing_table'][$id]['#weight'] = $badge->getBadgeWeight();
+
+      // Some table columns containing raw markup.
+      $form['badge_listing_table'][$id]['label'] = array(
+        '#plain_text' => $badge->label(),
+      );
+      $form['badge_listing_table'][$id]['id'] = array(
+        '#plain_text' => $badge->id(),
+      );
+
+      // TableDrag: Weight column element.
+      $form['badge_listing_table'][$id]['weight'] = array(
+        '#type' => 'weight',
+        '#title' => t('Weight for @title', array('@title' => $badge->label())),
+        '#title_display' => 'invisible',
+        '#default_value' => $badge->getBadgeWeight(),
+        '#attributes' => array('class' => array('user-badges-order-weight')),
+      );
+    }
+
+    $form['actions'] = array(
+      '#tree' => FALSE,
+      '#type' => 'actions',
+    );
+    $form['actions']['submit'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Save Badges'),
+      '#button_type' => 'primary',
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    // No validation.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+  // No Submit
   }
 
 }
